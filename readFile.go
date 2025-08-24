8bf105349ad5dc9b9b0d7f2655e51fc472725d3d
giorgio tassinari <giorgiobiologist@gmail.com>
2025-08-24 12:23:01 +0200

    added check null

--- readFile.go
+++ readFile.go
@@ -1,12 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func ReadFile() {
 	f, err := os.Open("message.txt")
 
-	if err /= nil {
-		ReadFile("message.txt")
-		fmt.Println(string(content))
+	if err != nil {
+		log.Fatal("error", "error", err)
 	}
 
-	
+	for {
+		dta := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+		fmt.Println("Read: %s\n", string(data[:n]))
+	}
+
+	//ReadFile("message.txt")
+	//fmt.Println(string(content))
 }
-
-
